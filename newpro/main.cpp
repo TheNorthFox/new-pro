96a4ed9316362c9102b44d05d942d4f1d5c4acfa
yegof <yegofm@yahoo.com>
2015-12-22 21:18:40 -0800

    Initial Commit

---
+++ newpro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  newpro
+//
+//  Created by minyahil kebebegn on 12/22/15.
+//  Copyright © 2015 minyahil kebebegn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
